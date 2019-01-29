3039022ba1ffb1c2469dde4d8d1f5977f9984019
Khabibullin <mkhabib@merann.ru>
2019-01-29 19:57:58 +0300

    mobx with decorators

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react';
 import logo from './logo.svg';
 import './App.css';
+import { observable } from 'mobx';
 
+@observer
 class App extends Component {
+  @observable counter = 0;
+
+  onIncrement = () => {
+    this.counter++;
+  }
+
+  onDecrement = () => {
+    this.counter--;
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.counter}
+
+        <button onClick={this.onIncrement} type="button">Increment</button>
+        <button onClick={this.onDecrement} type="button">Decrement</button>
       </div>
     );
   }
