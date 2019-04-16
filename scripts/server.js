217e2182da7dffa4c6654dbdb48ceb56cbcdde4f
Хабибуллин Марат Мансурович <marat-011@yandex.ru>
2019-04-16 16:38:41 +0300

    +

--- scripts/server.js
+++ scripts/server.js
@@ -19,8 +19,8 @@ app.all("/api/*", function(req, res) {
 });
 
 https.createServer({
-  key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.pem'),
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
   passphrase: '143265398'
 }, app)
 .listen(80);
