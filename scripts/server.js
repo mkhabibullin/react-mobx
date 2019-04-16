e48a5bf5c3564da76b67626d0d866e82cf263161
Хабибуллин Марат Мансурович <marat-011@yandex.ru>
2019-04-16 17:12:12 +0300

    +

--- scripts/server.js
+++ scripts/server.js
@@ -4,23 +4,34 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../', 'build')));
+try
+{
+  app.use(express.static(path.join(__dirname, '../', 'build')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
-});
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
+  });
 
-var httpProxy = require('http-proxy');
-var apiProxy = httpProxy.createProxyServer();
-var ocelot = 'http://localhost:5001/api';
+  var httpProxy = require('http-proxy');
+  var apiProxy = httpProxy.createProxyServer();
+  var ocelot = 'http://localhost:5001/api';
 
-app.all("/api/*", function(req, res) {
-  apiProxy.web(req, res, {target: ocelot});
-});
+  app.all("/api/*", function(req, res) {
+    apiProxy.web(req, res, {target: ocelot});
+  });
 
-https.createServer({
-  key: fs.readFileSync('./cert.key'),
-  cert: fs.readFileSync('./cert.pem'),
-  passphrase: '143265398'
-}, app)
-.listen(80);
+  // https.createServer({
+  //   key: fs.readFileSync('./cert.key'),
+  //   cert: fs.readFileSync('./cert.pem'),
+  //   passphrase: '143265398'
+  // }, app)
+  app.listen(80);
+} catch(exc) {
+  fs.writeFile("C://tmp/test", exc, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
+}
