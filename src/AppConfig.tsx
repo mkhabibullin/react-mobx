36e8e7a477eea99eff29bda5ec9df4cabe093bc8
Хабибуллин Марат Мансурович <marat-011@yandex.ru>
2019-04-16 17:57:08 +0300

    +

--- src/AppConfig.tsx
+++ src/AppConfig.tsx
@@ -1,4 +1,4 @@
 export default class AppConfig
 {
-    public static ApiUrl = process.env.NODE_ENV == 'production' ? 'https://i2x2.net/api' : 'http://localhost:5001/api';
+    public static ApiUrl = process.env.NODE_ENV == 'production' ? 'https://i2x2.net/api/app' : 'http://localhost:5001/api/app';
 }
