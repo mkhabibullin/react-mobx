3bc992b86b6fe9dd0be2c7862e4e19026d460410
Хабибуллин Марат Мансурович <marat-011@yandex.ru>
2019-05-15 12:20:44 +0300

    GA +

--- src/components/GA/index.tsx
+++ src/components/GA/index.tsx
@@ -6,20 +6,27 @@ class GA extends React.Component<any,any> {
     
     onFirstClick = () => {
         ReactGA.event({
-            category: 'User interaction',
+            category: 'Android',
             action: 'Check-In',
-            label: 'Android',
-            value: new Date().getTime()
+            label: this.getLabel()
           });
     }
 
     onSecondClick = () => {
         ReactGA.event({
-            category: 'User interaction',
+            category: 'IOS',
             action: 'Check-In',
-            label: 'IOS',
-            value: new Date().getTime()
+            label: this.getLabel()
           });
+    }
+
+    getLabel = () => this.uuidv4() + ' ' + new Date().toISOString()
+
+    uuidv4 = () => {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
     }
 
     render() {                
