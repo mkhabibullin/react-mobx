89d9d30e905837c9c839c108f0327aad8f7fd3ea
mkhabibullin <safe.kzn@gmail.com>
2019-07-20 11:23:35 +0300

    Fix a starting of web app

--- scripts/service.js
+++ scripts/service.js
@@ -14,8 +14,20 @@ var svc = new Service({
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
-svc.on('install',function(){
-  svc.start();
+svc.on('install',function() {
+  console.log('Installation complete.');
+	svc.start();
 });
 
-svc.install();
+// Listen for the "uninstall" event so we know when it's done.
+svc.on('uninstall',function() {
+  console.log('Uninstall complete.');
+  console.log('The service exists: ',svc.exists);
+  svc.install();
+});
+
+if (svc.exists) {
+  svc.uninstall();
+} else {
+  svc.install();
+}
